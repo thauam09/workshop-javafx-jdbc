e3e786fcb2267c194b76c6ec9e3a69c3f355618c
unknown <thauam_09martins@hotmail.com>
2021-10-05 01:34:04 -0300

    Adding database access

---
+++ src/db/DB.java
@@ -0,0 +1,67 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		try {
+			if (conn == null) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			return conn;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		try {
+			if (st != null) {
+				st.close();
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
